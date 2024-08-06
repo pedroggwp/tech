e8b37b2d31d8f145962d98b8ca3d4229344bd879
pedroggwp <phsnnascimento@outlook.com>
2024-08-06 11:43:00 -0300

    exportando módulos de forma ESM

--- DAD/aulaNode1/index.js
+++ DAD/aulaNode1/index.js
@@ -2,5 +2,12 @@ console.log("Olá mundo")
 
 // Common JS
 const matematica = require("./matematica")
+
 console.log(matematica.soma(5, 3));
 console.log(matematica.subtracao(5, 3));
+
+// ESM
+import { soma, subtracao } from "./matematica_es.js"
+
+console.log(soma(3, 2))
+console.log(subtracao(3, 2))
---
+++ DAD/aulaNode1/matematica_es.js
@@ -0,0 +1,2 @@
+export const soma = (a, b) => a + b;
+export const subtracao = (a, b) => a - b;
