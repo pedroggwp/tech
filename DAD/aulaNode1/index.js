01d772e85892d01578ee442462ae95c63da87bc7
pedroggwp <phsnnascimento@outlook.com>
2024-08-06 08:30:22 -0300

    aula de node - exportação e importação de funções

---
+++ DAD/aulaNode1/index.js
@@ -0,0 +1,6 @@
+console.log("Olá mundo")
+
+// Common JS
+const matematica = require("./matematica")
+console.log(matematica.soma(5, 3));
+console.log(matematica.subtracao(5, 3));
---
+++ DAD/aulaNode1/matematica.js
@@ -0,0 +1,4 @@
+const soma = (a, b) => a + b;
+const subtracao = (a, b) => a - b;
+
+module.exports = {soma, subtracao}
